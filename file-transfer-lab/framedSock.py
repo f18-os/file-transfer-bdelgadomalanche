aa9f07c655fa2c4703d83160f490a22244e46f38
Brandon Delgado Malanche <bdelgadomalanche@miners.utep.edu>
2018-10-02 09:04:28 -0600

    Intial Push, work in progress

---
+++ file-transfer-lab/framedSock.py
@@ -0,0 +1,43 @@
+import re
+
+def framedSend(sock, payload, debug=0):
+     if debug: print("framedSend: sending %d byte message" % len(payload))
+     msg = str(len(payload)).encode() + b':' + payload
+     while len(msg):
+         nsent = sock.send(msg)
+         msg = msg[nsent:]
+         
+rbuf = b""                      # static receive buffer
+
+def framedReceive(sock, debug=0):
+    global rbuf
+    state = "getLength"
+    msgLength = -1
+    while True:
+         print(state)
+         if (state == "getLength"):
+             print(rbuf)
+             match = re.match(b'([^:]+):(.*)', rbuf) # look for colon
+             print(match)
+             if match:
+                  lengthStr, rbuf = match.groups()
+                  try: 
+                       msgLength = int(lengthStr)
+                       print(msgLength)
+                  except:
+                       if len(rbuf):
+                            print("badly formed message length:", lengthStr)
+                            return None
+                  state = "getPayload"
+         if state == "getPayload":
+             if len(rbuf) >= msgLength:
+                 payload = rbuf[0:msgLength]
+                 rbuf = rbuf[msgLength:]
+                 return payload
+         r = sock.recv(100)
+         rbuf += r
+         if len(r) == 0:
+             if len(rbuf) != 0:
+                 print("FramedReceive: incomplete message. \n  state=%s, length=%d, rbuf=%s" % (state, msgLength, rbuf))
+             return None
+         if debug: print("FramedReceive: state=%s, length=%d, rbuf=%s" % (state, msgLength, rbuf))
